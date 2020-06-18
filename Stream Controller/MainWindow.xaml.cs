fd56f7281a4ca6536fba4cd8bd9a2b825da88cfc
John Cook <watfordjc@users.noreply.github.com>
2020-06-18 18:16:59 +0100

    Early code

---
+++ Stream Controller/MainWindow.xaml.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+
+namespace Stream_Controller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
+        private readonly AudioEndpointNotificationCallback notificationCallback;
+        private readonly IMMNotificationClient notificationClient;
+        private readonly System.Collections.ObjectModel.ObservableCollection<String> devices = new System.Collections.ObjectModel.ObservableCollection<string>();
+        private MMDevice defaultRenderDevice;
+        private MMDevice defaultCaptureDevice;
+        private MMDevice currentInterface;
+        public MainWindow()
+        {
+            InitializeComponent();
+            notificationCallback = new AudioEndpointNotificationCallback(this);
+            notificationClient = (IMMNotificationClient)notificationCallback;
+            deviceEnumerator.RegisterEndpointNotificationCallback(notificationClient);
+            PopulateAudioInterfaces();
+        }
+
+        private void PopulateAudioInterfaces()
+        {
+            foreach (MMDevice device in deviceEnumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active | DeviceState.Unplugged | DeviceState.NotPresent))
+            {
+                devices.Add(device.ID);
+                if (device.State != DeviceState.NotPresent)
+                {
+                    cb_interfaces.Items.Add(device.FriendlyName.ToString());
+                    continue;
+                }
+                cb_interfaces.Items.Add(device.ID.ToString());
+            }
+            cb_interfaces.SelectionChanged += CurrentDeviceChanged;
+            defaultRenderDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+            DefaultRenderDeviceChanged();
+            defaultCaptureDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
+            DefaultCaptureDeviceChanged();
+        }
+
+        private void ChangeDefaultRenderDevice(string defaultDeviceId)
+        {
+            defaultRenderDevice.AudioEndpointVolume.OnVolumeNotification -= OnDefaultRenderDeviceVolumeNotification;
+            defaultRenderDevice = deviceEnumerator.GetDevice(defaultDeviceId);
+            DefaultRenderDeviceChanged();
+        }
+
+        private void OnDefaultRenderDeviceVolumeNotification(AudioVolumeNotificationData data)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                render_interface_volume.Text = Math.Round(defaultRenderDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100f).ToString() + "%";
+                render_interface_muted.Text = defaultRenderDevice.AudioEndpointVolume.Mute == true ? "Yes" : "No";
+            });
+        }
+
+        private void ChangeDefaultCaptureDevice(string defaultDeviceId)
+        {
+            defaultCaptureDevice.AudioEndpointVolume.OnVolumeNotification -= OnDefaultCaptureDeviceVolumeNotification;
+            defaultCaptureDevice = deviceEnumerator.GetDevice(defaultDeviceId);
+            DefaultCaptureDeviceChanged();
+        }
+
+        private void OnDefaultCaptureDeviceVolumeNotification(AudioVolumeNotificationData data)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                capture_interface_volume.Text = Math.Round(defaultCaptureDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100f).ToString() + "%";
+                capture_interface_muted.Text = defaultCaptureDevice.AudioEndpointVolume.Mute == true ? "Yes" : "No";
+            });
+        }
+
+        private void OnCurrentInterfaceVolumeNotification(AudioVolumeNotificationData data)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                interface_volume.Text = Math.Round(currentInterface.AudioEndpointVolume.MasterVolumeLevelScalar * 100f).ToString() + "%";
+                interface_muted.Text = data.Muted == true ? "Yes" : "No";
+            });
+        }
+
+        private class AudioEndpointNotificationCallback : NAudio.CoreAudioApi.Interfaces.IMMNotificationClient
+        {
+            private readonly MainWindow mWindow;
+            public AudioEndpointNotificationCallback(MainWindow window)
+            {
+                this.mWindow = window;
+            }
+
+            public void OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId)
+            {
+                if (role != Role.Console)
+                {
+                    return;
+                }
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    if (flow == DataFlow.Render)
+                    {
+                        mWindow.ChangeDefaultRenderDevice(defaultDeviceId);
+                    }
+                    else if (flow == DataFlow.Capture)
+                    {
+                        mWindow.ChangeDefaultCaptureDevice(defaultDeviceId);
+                    }
+                });
+            }
+
+            public void OnDeviceAdded(string pwstrDeviceId)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    if (mWindow.devices.Contains(pwstrDeviceId))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        mWindow.devices.Add(pwstrDeviceId);
+                        mWindow.cb_interfaces.Items.Add(mWindow.deviceEnumerator.GetDevice(pwstrDeviceId).FriendlyName);
+                    }
+                });
+            }
+
+            public void OnDeviceRemoved(string deviceId)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    if (!mWindow.devices.Contains(deviceId))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        int old_index = -1;
+                        if (mWindow.currentInterface == mWindow.deviceEnumerator.GetDevice(deviceId))
+                        {
+                            old_index = mWindow.devices.IndexOf(mWindow.currentInterface.ID);
+                            mWindow.currentInterface = mWindow.defaultRenderDevice;
+                            mWindow.cb_interfaces.SelectedIndex = mWindow.devices.IndexOf(mWindow.currentInterface.ID);
+                        }
+                        mWindow.devices.RemoveAt(old_index);
+                        mWindow.cb_interfaces.Items.RemoveAt(old_index);
+                    }
+                });
+            }
+
+            public void OnDeviceStateChanged(string deviceId, DeviceState newState)
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    DeviceState oldState = mWindow.deviceEnumerator.GetDevice(deviceId).State;
+                    int cb_selected = mWindow.cb_interfaces.SelectedIndex;
+                    int index = -1;
+                    if (mWindow.cb_interfaces.Items.Contains(deviceId))
+                    {
+                        index = mWindow.cb_interfaces.Items.IndexOf(deviceId);
+                        if (newState == DeviceState.Active || newState == DeviceState.Unplugged)
+                        {
+                            mWindow.cb_interfaces.Items[index] = mWindow.deviceEnumerator.GetDevice(deviceId).FriendlyName;
+                        }
+                    } else if (newState != DeviceState.Active && newState != DeviceState.Unplugged)
+                    {
+                        index = mWindow.devices.IndexOf(deviceId);
+                        mWindow.cb_interfaces.Items[index] = deviceId;
+                    }
+                    if (cb_selected == index)
+                    {
+                        mWindow.cb_interfaces.SelectedIndex = index;
+                    }
+                });
+                return;
+            }
+
+            public void OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key)
+            {
+                return;
+                // throw new NotImplementedException();
+            }
+        }
+
+        private void DefaultRenderDeviceChanged()
+        {
+            int device_index = devices.IndexOf(defaultRenderDevice.ID);
+            cb_interfaces.SelectedIndex = device_index;
+            render_interface_name.Text = defaultRenderDevice.FriendlyName;
+            render_interface_type.Text = defaultRenderDevice.DataFlow.ToString();
+            render_interface_state.Text = defaultRenderDevice.State.ToString();
+            render_interface_volume.Text = Math.Round(defaultRenderDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100f).ToString() + "%";
+            defaultRenderDevice.AudioEndpointVolume.OnVolumeNotification += OnDefaultRenderDeviceVolumeNotification;
+            render_interface_muted.Text = defaultRenderDevice.AudioEndpointVolume.Mute == true ? "Yes" : "No";
+        }
+
+        private void DefaultCaptureDeviceChanged()
+        {
+            capture_interface_name.Text = defaultCaptureDevice.FriendlyName;
+            capture_interface_type.Text = defaultCaptureDevice.DataFlow.ToString();
+            capture_interface_state.Text = defaultCaptureDevice.State.ToString();
+            capture_interface_volume.Text = Math.Round(defaultCaptureDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100f).ToString() + "%";
+            defaultCaptureDevice.AudioEndpointVolume.OnVolumeNotification += OnDefaultCaptureDeviceVolumeNotification;
+            capture_interface_muted.Text = defaultCaptureDevice.AudioEndpointVolume.Mute == true ? "Yes" : "No";
+        }
+
+        private void CurrentDeviceChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count == 0)
+            {
+                return;
+            }
+            if (e.RemovedItems.Count == 1 && currentInterface.State == DeviceState.Active)
+            {
+                currentInterface.AudioEndpointVolume.OnVolumeNotification -= OnCurrentInterfaceVolumeNotification;
+            }
+            //currentInterface = deviceEnumerator.GetDevice(devices[cb_interfaces.SelectedIndex]);
+            int new_index = (sender as ComboBox).SelectedIndex;
+            currentInterface = deviceEnumerator.GetDevice(devices[new_index]);
+            if (currentInterface.State != DeviceState.NotPresent)
+            {
+                interface_name.Text = currentInterface.FriendlyName;
+            } else
+            {
+                interface_name.Text = currentInterface.ID;
+            }
+            
+            interface_type.Text = currentInterface.DataFlow.ToString();
+            interface_state.Text = currentInterface.State.ToString();
+            if (currentInterface.State == DeviceState.Active)
+            {
+                interface_volume_label.Visibility = Visibility.Visible;
+                interface_volume.Text = Math.Round(currentInterface.AudioEndpointVolume.MasterVolumeLevelScalar * 100f).ToString() + "%";
+                interface_volume.Visibility = Visibility.Visible;
+                interface_muted_label.Visibility = Visibility.Visible;
+                interface_muted.Text = currentInterface.AudioEndpointVolume.Mute == true ? "Yes" : "No";
+                interface_muted.Visibility = Visibility.Visible;
+                currentInterface.AudioEndpointVolume.OnVolumeNotification += OnCurrentInterfaceVolumeNotification;
+            }
+            else
+            {
+                interface_volume_label.Visibility = Visibility.Collapsed;
+                interface_volume.Visibility = Visibility.Collapsed;
+                interface_muted_label.Visibility = Visibility.Collapsed;
+                interface_muted.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
---
+++ Stream Controller/WebSocketTest.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Stream_Controller
+{
+    /// <summary>
+    /// Interaction logic for WebSocketTest.xaml
+    /// </summary>
+    public partial class WebSocketTest : Window, IDisposable
+    {
+        private ClientWebSocket webSocket = null;
+        private bool autoscroll = false;
+        private bool disposedValue;
+
+        public WebSocketTest()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonTest_Click(object sender, RoutedEventArgs e)
+        {
+            webSocket = ClientWebSocket();
+            if (webSocket == null)
+            {
+                e.Handled = true;
+                return;
+            }
+            txtStatus.Text = webSocket.State.ToString();
+            if (webSocket.State == WebSocketState.Open)
+            btnTest.IsEnabled = false;
+            e.Handled = true;
+            await Task.WhenAll(new[] { ReceiveMessages() });
+            btnTest.IsEnabled = true;
+        }
+
+        private async void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (webSocket != null && webSocket.State != WebSocketState.Closed)
+            {
+                await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Requested by user", CancellationToken.None);
+                txtStatus.Text = webSocket.State.ToString();
+            }
+            e.Handled = true;
+        }
+
+        private void AutoScroll_Checked(object sender, RoutedEventArgs e)
+        {
+            autoscroll = true;
+            svScroll.ScrollToBottom();
+            svScroll.ScrollChanged += OnScrollChange;
+        }
+
+        private void AutoScroll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            autoscroll = false;
+        }
+
+        private void AutoScroll_Scrolled(object sender, RoutedEventArgs e)
+        {
+            cbAutoScroll.IsChecked = false;
+        }
+
+        private void OnScrollChange(object sender, ScrollChangedEventArgs e)
+        {
+            if (e.VerticalOffset < 0 && autoscroll == true)
+            {
+                cbAutoScroll.IsChecked = false;
+            }
+        }
+
+        private ClientWebSocket ClientWebSocket()
+        {
+            ClientWebSocket clientWebSocket = new ClientWebSocket();
+            try
+            {
+                clientWebSocket.Options.SetBuffer(8192, 8192);
+                clientWebSocket.ConnectAsync(new Uri("ws://localhost:4444"), CancellationToken.None).GetAwaiter().GetResult();
+            } catch (WebSocketException e)
+            {
+                txtOutput.Text = "An error occurred: " + e.Message + "\n\n" + e.InnerException.Message;
+                return null;
+            }
+            
+            return clientWebSocket;
+        }
+
+        private async Task ReceiveMessages()
+        {
+            while (webSocket.State == WebSocketState.Open)
+            {
+                await ReceiveAsync();
+            }
+        }
+
+        private async Task SendAsync(string message)
+        {
+            if (webSocket.State != WebSocketState.Open)
+            {
+                return;
+            }
+            ArraySegment<Byte> sendBuffer = new ArraySegment<Byte>(Encoding.UTF8.GetBytes(message));
+            await webSocket.SendAsync(sendBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        // TODO: Licensing - function derived from https://stackoverflow.com/questions/23773407/a-websockets-receiveasync-method-does-not-await-the-entire-message
+        private async Task<WebSocketReceiveResult> ReceiveAsync()
+        {
+            ArraySegment<Byte> receiveBuffer = new ArraySegment<Byte>(new Byte[8192]);
+            WebSocketReceiveResult result = null;
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                try
+                {
+                    do
+                    {
+                        result = await webSocket.ReceiveAsync(receiveBuffer, CancellationToken.None);
+                        memoryStream.Write(receiveBuffer.Array, receiveBuffer.Offset, result.Count);
+                    } while (!result.EndOfMessage);
+                } catch (OutOfMemoryException e)
+                {
+                    txtOutput.Text += "Unable to receive a WebSocket message due to a buffer overflow.\n" + e.Message + "\n" + e.InnerException.Message + "\n";
+                }
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    using StreamReader streamReader = new StreamReader(memoryStream, Encoding.UTF8);
+                    txtStatus.Text = webSocket.State.ToString();
+                    txtOutput.Text += streamReader.ReadToEnd() + "\n\n";
+                    if (autoscroll == true)
+                    {
+                        svScroll.ScrollToBottom();
+                    }
+                }
+                else if (result.MessageType == WebSocketMessageType.Binary)
+                {
+                    throw new NotImplementedException("Binary WebSocket messages not implemented.");
+                }
+            }
+            return result;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Program exiting", CancellationToken.None).GetAwaiter().GetResult();
+                webSocket.Dispose();
+
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~WebSocketTest()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
