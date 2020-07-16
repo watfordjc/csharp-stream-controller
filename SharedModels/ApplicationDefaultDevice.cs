1d4f8507c1f7b31dcfb7cdf69643dedeb38e187c
John Cook <watfordjc@users.noreply.github.com>
2020-07-16 19:05:42 +0100

    Create JSON models for application-device mapping

---
+++ SharedModels/ApplicationDefaultDevice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace uk.JohnCook.dotnet.SharedModels
+{
+    public class ApplicationDevicePreferences
+    {
+        [JsonPropertyName("applications")]
+#pragma warning disable CA2227 // Collection properties should be read only
+        public IList<ApplicationDevicePreference> Applications { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
+    }
+
+    public class ApplicationDevicePreference
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("devices")]
+        public DefaultDevicePreference Devices { get; set; }
+    }
+
+    public class DefaultDevicePreference
+    {
+        [JsonPropertyName("render")]
+        public string RenderDeviceId { get; set; }
+        [JsonPropertyName("capture")]
+        public string CaptureDeviceId { get; set; }
+    }
+}
---
+++ SharedModels/DeviceApplications.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace uk.JohnCook.dotnet.SharedModels
+{
+    public class DeviceApplicationPreferences
+    {
+        [JsonPropertyName("devices")]
+#pragma warning disable CA2227 // Collection properties should be read only
+        public IList<DeviceApplicationPreference> Devices { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
+    }
+
+    public class DeviceApplicationPreference
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("applications")]
+#pragma warning disable CA2227 // Collection properties should be read only
+        public IList<string> Applications { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
+    }
+}
