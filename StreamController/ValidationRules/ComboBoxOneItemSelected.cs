5865fe0ebb9ca5886d667e4949fd4faa1cf9dc69
John Cook <watfordjc@users.noreply.github.com>
2020-07-21 17:49:22 +0100

    Make validation real-time, validate URI scheme not deselected

---
+++ StreamController/ValidationRules/ComboBoxOneItemSelected.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace uk.JohnCook.dotnet.StreamController.ValidationRules
+{
+    public class ComboBoxOneItemSelected : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null) {
+                return new ValidationResult(false, "Please select an item.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
