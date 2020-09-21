7c9ff87aa26e51d5a2292e855e87b84e00846bc8
John Cook <coder-a599-7927@johncook.co.uk>
2020-09-21 20:55:11 +0100

    Add dispose pattern to VerticalMessagePanel

--- StreamController/MessagePanel.cs
+++ StreamController/MessagePanel.cs
@@ -21,6 +21,7 @@ namespace uk.JohnCook.dotnet.StreamController
         private readonly MessagePanel verticalMessagePanel;
         private PointF messageBlankingAreaOriginPoint;
         private RectF messageBlankingArea;
+        private bool disposedValue;
 
         public VerticalMessagePanel()
         {
@@ -463,12 +464,37 @@ namespace uk.JohnCook.dotnet.StreamController
             Trace.WriteLine($"Image updated.");
             return true;
         }
-        #endregion
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    verticalMessagePanel.Dispose();
+                    direct2DWrapper.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~VerticalMessagePanel()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
 
         public void Dispose()
         {
-            verticalMessagePanel.Dispose();
-            direct2DWrapper.Dispose();
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
+        #endregion
     }
 }
